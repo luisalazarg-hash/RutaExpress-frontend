c80bdf443388ddf592ca9e3fa7f5766076bb283b
NicoAranda <n.aranda@duocuc.cl>
2026-09-17 21:14:52 -0300

    cambios navbar no definitivos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css'
+import logo from '../assets/logo.png'
 
 export const NavBar = () => {
+
+	const [scrolled, setScrolled] = useState(false)
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrolled(window.scrollY > 50)
+		}
+
+		window.addEventListener('scroll', handleScroll)
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
+
 	return (
 		<>
-			<nav className="navbar navbar-expand-lg navbar-dark p-4">
-				<div className="container-fluid d-flex justify-content-between">
-					<h1 className="navbar-brand mb-0 fs-2" href="#">Ruta <span className='text-danger'>Express</span></h1>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a className="nav-link active" aria-current="page" href="#">Inicio</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Ver Pedidos</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Iniciar Sesion</a>
-							</li>
-							
-						</ul>
-					</div>
+			<nav className={`navbar fixed-top ${scrolled ? 'navbar-scrolled' : 'navbar-hero'}`}>
+				<div className="container">
+
+					{!scrolled && (
+						<div className='logo'>
+							<a className="navbar-brand" href="#up">
+								<img src={logo} alt="RutaExpress" className="logo" />
+							</a>
+						</div>
+					)}
+
+					<ul className="navbar-nav ms-auto flex-row gap-4">
+						<li className="nav-item">
+							<a className="nav-link" href="#up">Inicio</a>
+						</li>
+
+						<li className="nav-item">
+							<a className="nav-link" href="#about">Nosotros</a>
+						</li>
+
+						<li className="nav-item">
+							<a className="nav-link" href="#services">Servicios</a>
+						</li>
+
+						<li className="nav-item">
+							<a className="nav-link" href="#contact">Contacto</a>
+						</li>
+					</ul>
+
 				</div>
 			</nav>
 		</>
