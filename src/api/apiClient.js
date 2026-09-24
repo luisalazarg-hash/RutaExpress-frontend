e34c5cb00df5731c3a7cd329092ef98a85a6efb1
Tvbaal <cristo.perezr@duocuc.cl>
2026-09-23 23:18:48 -0300

    update

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:8088'
+const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:8080'
 
 export function createApiClient(instance, account) {
   async function getToken() {
